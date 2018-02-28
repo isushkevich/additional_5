0919aa36d022fcfc83c57c2744e95888d80fa317
isushkevich <ivan.sushik@gmail.com>
2018-03-01 00:45:21 +0300

    17/20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check( str, bracketsConfig ) {
+  let stack = [];
+
+  for ( let i = 0; i < str.length; ++i ) {
+
+    if ( str[ i ] == '{' || str[ i ] == '(' || str[ i ] == '[' ) {
+      stack.push( str[ i ] );
+    }
+
+    if ( str[ i ] == '}' ) {
+      if ( stack[ stack.length - 1 ] == '{' ) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+
+    if ( str[ i ] == ')' ) {
+      if ( stack[ stack.length - 1 ] == '(' ) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+
+    if ( str[ i ] == ']' ) {
+      if ( stack[ stack.length - 1 ] == '[' ) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  if ( stack.length == 0 ) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
